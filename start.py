87688fcc031bf8f4f1708ab17a2aa1239409372b
Kyrylo <140982410+KyryloChalov@users.noreply.github.com>
2024-03-21 01:03:52 +0200

    add start.py

---
+++ start.py
@@ -0,0 +1,19 @@
+import os
+
+
+commands = [
+    # "poetry shell",
+    "poetry update package",
+    "docker pull postgres",
+    "docker-compose up -d",
+    "uvicorn main:app --host localhost --port 8000 --reload",
+]
+
+
+def run_command(command):
+    print("Running command: {}".format(command))
+    os.system(command)
+
+
+for command in commands:
+    run_command(command)
