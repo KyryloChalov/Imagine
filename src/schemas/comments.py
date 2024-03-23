1f30f18b34b981d91aa3714ec8de4364c313bd10
OleksiyM <oleksiymaliovanyy@gmail.com>
2024-03-23 12:29:43 +0200

    Added constants.py comments

--- src/schemas/comments.py
+++ src/schemas/comments.py
@@ -4,38 +4,16 @@ from typing import Optional
 from datetime import datetime, date
 
 from pydantic import BaseModel, EmailStr, Field, ConfigDict, validator
+from src.conf.constants import COMMENT_MIN_LENGTH, COMMENT_MAX_LENGTH
 
 
-class ContactSchema(BaseModel):
-    name: str = Field(max_length=30)
-    surname: str = Field(max_length=30)
-    email: EmailStr = Field(max_length=80)
-    birthday: Optional[date] = Field(None)
-    phone:  Optional[str] = Field(max_length=20)
-    info: Optional[str] = Field(max_length=200, default=None)
-    
-    @validator("phone")
-    def phone_number(cls, phone_num):
-        match = re.match(r"\d{12}", phone_num)
-        if (match is None) or (len(phone_num) != 12):
-            print(match, len(phone_num), phone_num)
-            raise ValueError("Phone number must have 12 digits")
-        return phone_num
+class CommentSchema(BaseModel):
+    pass
 
 
-class ContactUpdateSchema(ContactSchema):
-    created_at: datetime = Field(default=datetime.now())
+class CommentResposeSchema(BaseModel):
+    pass
 
-class ContactResponse(BaseModel):
-    id: int = 1
-    name: str
-    surname: str
-    email: EmailStr
-    birthday: date
-    phone: str
-    info: str | None
-    
-    model_config = ConfigDict(from_attributes = True)  # noqa
 
-    # class Config:
-    #     from_attributes = True
+class CommentUpdateSchema(BaseModel):
+    pass
