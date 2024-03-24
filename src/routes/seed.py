93a934999fe1d16ccf8f7449cd30544647a79265
KyryloChalov <ks7977166@gmail.com>
2024-03-24 23:56:39 +0200

    add seed rating

--- src/routes/seed.py
+++ src/routes/seed.py
@@ -3,6 +3,7 @@ from src.seed.users import seed_users, seed_basic_users
 from src.seed.tags import seed_tags
 from src.seed.photos import seed_photos
 from src.seed.comments import seed_comments
+from src.seed.ratings import seed_ratings
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.db import get_db
 
@@ -41,3 +42,9 @@ async def seed_fake_comments(
 ):
     await seed_comments(number_comments, db)
     return {"message": f"You have {number_comments} new fake Comments"}
+
+
+@router.post("/fake_ratings")
+async def seed_fake_ratings(db: AsyncSession = Depends(get_db)):
+    await seed_ratings(db)
+    return {"message": f"You have new fake Ratings"}
--- src/seed/ratings.py
+++ src/seed/ratings.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from src.database.db import get_db
-from src.models.models import Photo, User, Comment
+from src.models.models import Photo, User, Rating
 from src.conf.constants import COMMENT_MIN_LENGTH, COMMENT_MAX_LENGTH
 
 import random
@@ -12,7 +12,7 @@ import random
 fake_data: Faker = Faker(["uk_UA", "en_US"])
 
 
-async def seed_comments(count: int = 100, db: AsyncSession = Depends(get_db)):
+async def seed_ratings(db: AsyncSession = Depends(get_db)):
     """
     генерація кількох фейкових comments (за замовченням: count: int = 100)
     """
@@ -30,15 +30,14 @@ async def seed_comments(count: int = 100, db: AsyncSession = Depends(get_db)):
         photos_id.append(photo.id)
     # print(f"{photos_id = }")
 
+    count = len(users_id) * len(photos_id) // 2
     for _ in range(count):
-        new_comment = Comment(
-            opinion=fake_data.text(
-                random.randint(COMMENT_MIN_LENGTH, COMMENT_MAX_LENGTH)
-            )[:-1],
+        new_rating = Rating(
+            rating=random.randint(1, 5),
             user_id=users_id[random.randint(0, len(users_id) - 1)],
             photo_id=photos_id[random.randint(0, len(photos_id) - 1)],
         )
 
-        db.add(new_comment)
+        db.add(new_rating)
         await db.commit()
-        await db.refresh(new_comment)
+        await db.refresh(new_rating)
