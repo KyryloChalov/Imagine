164b5c99ef4ca1f89f729d5e30444a5f7a823808
OleksiyM <oleksiymaliovanyy@gmail.com>
2024-03-24 16:47:18 +0200

    added comments

--- src/schemas/comments.py
+++ src/schemas/comments.py
@@ -1,20 +1,22 @@
-"""скопіював з contacts"""
-
-import re
 from typing import Optional
 from datetime import datetime, date
 
 from pydantic import BaseModel, EmailStr, Field, ConfigDict, validator
 from src.conf.constants import COMMENT_MIN_LENGTH, COMMENT_MAX_LENGTH
 
+import uuid
+
 
 class CommentSchema(BaseModel):
-    pass
+    opinion: str = Field(min_length=COMMENT_MIN_LENGTH, max_length=COMMENT_MAX_LENGTH)
 
 
 class CommentResposeSchema(BaseModel):
-    pass
+    id: int
+    opinion: str = Field(min_length=COMMENT_MIN_LENGTH, max_length=COMMENT_MAX_LENGTH)
+    user_id: uuid.UUID
+    photo_id: int
 
 
 class CommentUpdateSchema(BaseModel):
-    pass
+    opinion: str = Field(min_length=COMMENT_MIN_LENGTH, max_length=COMMENT_MAX_LENGTH)
