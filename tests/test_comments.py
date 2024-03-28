56a2cac118d47c1bbb37bc495f5f62be5294bd3d
UreshiiSushi <merieru@gmail.com>
2024-03-28 12:55:05 +0100

    added schemes for tests

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,7 +1,25 @@
+from unittest.mock import Mock, patch, AsyncMock
 
+import pytest
 
-#TODO test create comment
-#TODO test get all comments
-#TODO test get user comments by user id
-#TODO test update comment by comment id
-#TODO test delete comment
+from src.services.auth import auth_service
+
+# TODO test create comment
+def test_create_comment():
+    pass
+
+# TODO test get all comments
+def test_get_comments():
+    pass
+
+# TODO test get user comments by user id
+def test_get_user_comments():
+    pass
+
+# TODO test update comment by comment id
+def test_update_comment():
+    pass
+
+# TODO test delete comment
+def test_delete_comment():
+    pass
--- tests/test_e2e_user.py
+++ tests/test_e2e_user.py
@@ -17,9 +17,26 @@ def test_get_me(client, get_token, monkeypatch):
         assert response.status_code == 200, response.text
 
 #TODO test update avatar
+def test_update_avatar():
+    pass
+
 #TODO test get all users
+def test_get_all_users(client, get_token, monkeypatch):
+    pass
+
 #TODO test update user
+def test_update_user():
+    pass
+
 #TODO test change user role
+def test_change_user_role():
+    pass
+
 #TODO test get some user info
+def test_get_some_user_info():
+    pass
+
 #TODO test delete user
-        
+def test_delete_user():
+    pass
+
