271a7f708cde727d38c2b01821ed575730f9912d
Kyrylo <140982410+KyryloChalov@users.noreply.github.com>
2024-04-01 03:22:12 +0300

    Update form.js

--- templates/js/form.js
+++ templates/js/form.js
@@ -1,9 +1,6 @@
 const form = document.querySelector('#form');
-// const readme = document.querySelector('#readme');
 const launchBtn = document.querySelector('#launch-btn');
 const goToFormButton = document.querySelector('#go-to-form-btn');
-// const goToReadmeButton = document.querySelector('#go-to-readme-btn');
-
 
 
 goToFormButton.addEventListener('click', function (e) {
@@ -16,14 +13,3 @@ goToReadmeButton.addEventListener('click', function (e) {
     e.preventDefault();
     form.scrollIntoView();
 });
-
-
-// const launchBtn = document.querySelector('#launch-btn');
-// const goToFormButton = document.querySelector('#go-to-three-btn');
-
-
-
-// goToFormButton.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     form.scrollIntoView();
-// });
