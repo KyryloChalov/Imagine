6ac88c9f740ee899ae10ee52509fee2fa4145f06
KyryloChalov <ks7977166@gmail.com>
2024-04-04 21:27:02 +0300

    fix test_photos from Masha

--- tests/test_photos.py
+++ tests/test_photos.py
@@ -96,17 +96,27 @@ class TestPhotos(unittest.IsolatedAsyncioTestCase):
         result = await get_photo_by_id(photo_id, self.session)
         self.assertTrue(result)
 
-    # failed
-    async def test_create_photo(self):
+    # # failed
+    # async def test_create_photo(self):
+    #     tags = ["tag1", "tag2"]
+    #     mocked_user = MagicMock()
+    #     mocked_user.username = self.user.username
+    #     mocked_user.first.return_value = self.user
+    #     mocked_photo = MagicMock()
+    #     mocked_photo.first.return_value = None
+    #     # result = await create_photo("test.jpg", self.photo.description, mocked_user.id, self.session, tags)
+    #     # self.assertEqual(result["success message"], PHOTO_SUCCESSFULLY_ADDED)
+
+    @patch("cloudinary.uploader.upload")
+    async def test_create_photo(self, patch):
         tags = ["tag1", "tag2"]
         mocked_user = MagicMock()
         mocked_user.username = self.user.username
         mocked_user.first.return_value = self.user
         mocked_photo = MagicMock()
-        mocked_photo.first.return_value = None
-        # result = await create_photo("test.jpg", self.photo.description, mocked_user.id, self.session, tags)
-        # self.assertEqual(result["success message"], PHOTO_SUCCESSFULLY_ADDED)
-
+        self.session.execute.return_value = mocked_user
+        result = await create_photo(mocked_photo, self.photo.description, mocked_user.id, self.session, tags)
+        self.assertEqual(result["success message"], PHOTO_SUCCESSFULLY_ADDED)
     # failed
 
     async def test_add_tag_to_photo(self):
@@ -119,26 +129,51 @@ class TestPhotos(unittest.IsolatedAsyncioTestCase):
         # result = await add_tag_to_photo(mocked_photo.id, tag, self.session)
         # self.assertEqual(result["success message"], TAG_SUCCESSFULLY_ADDED)
 
-    # failed
+    # # failed
+    # async def test_edit_photo_description(self):
+    #     description = "test"
+    #     mocked_photo = MagicMock()
+    #     mocked_photo.first.return_value = self.photo
+    #     self.session.execute.return_value = mocked_photo
+    #     result = await edit_photo_description(user=self.user, photo_id=self.photo.id, description=description,
+    #                                           db=self.session)
+    #     self.assertTrue(result)
+
     async def test_edit_photo_description(self):
-        description = "test"
+        description = "test_test"
         mocked_photo = MagicMock()
+        mocked_photo.id = 1
         mocked_photo.first.return_value = self.photo
         self.session.execute.return_value = mocked_photo
-        result = await edit_photo_description(user=self.user, photo_id=self.photo.id, description=description,
-                                              db=self.session)
+        result = await edit_photo_description(self.user, self.photo.id, description, self.session)
         self.assertTrue(result)
 
-    # failed
+    # # failed
+    # async def test_delete_photo(self):
+    #     mocked_photo = MagicMock()
+    #     mocked_photo.first.return_value = self.photo
+    #     self.session.execute.return_value = mocked_photo
+    #     result = await delete_photo(self.photo.id, self.user, self.session)
+    #     self.session.execute.assert_called()
+    #     # self.assertTrue(result)
+
     async def test_delete_photo(self):
         mocked_photo = MagicMock()
+        mocked_photo.id = 1
         mocked_photo.first.return_value = self.photo
         self.session.execute.return_value = mocked_photo
         result = await delete_photo(self.photo.id, self.user, self.session)
-        self.session.execute.assert_called()
-        # self.assertTrue(result)
+        self.assertIsNone(result)
 
-    # failed
+    # # failed
+    # async def test_del_photo_tag(self):
+    #     tag_for_del = "test"
+    #     mocked_photo = MagicMock()
+    #     mocked_photo.first.return_value = self.photo
+    #     self.session.execute.return_value = mocked_photo
+    #     result = await del_photo_tag(self.photo.id, tag_for_del, self.session)
+    #     self.assertTrue(result)
+
     async def test_del_photo_tag(self):
         tag_for_del = "test"
         mocked_photo = MagicMock()
@@ -155,26 +190,51 @@ class TestPhotos(unittest.IsolatedAsyncioTestCase):
         # result = await change_photo(self.user, self.photo.id, self.session, 100, 150, "fill", 'art')
         # self.assertTrue(result)
 
+    # async def test_make_avatar_from_photo(self):
+    #     mocked_photo = MagicMock()
+    #     mocked_photo.first.return_value = self.photo
+    #     result = await make_avatar_from_photo(self.user, self.photo.id, "art", self.session)
+    #     self.assertTrue(result)
+
     async def test_make_avatar_from_photo(self):
         mocked_photo = MagicMock()
         mocked_photo.first.return_value = self.photo
-        # result = await make_avatar_from_photo(self.user, self.photo.id, "art", self.session)
-        # self.assertTrue(result)
+        result = await make_avatar_from_photo(self.user, self.photo.id, "art", self.session)
+        self.assertTrue(result)
 
-    # failed
+    # # failed
+    # async def test_search_photos(self):
+    #     photos = [Photo(), Photo(), Photo()]
+    #     mocked_photos = MagicMock()
+    #     mocked_photos.scalars.return_value.all.return_value = photos
+    #     # result = await search_photos(self.user, "test", 10, 0, self.session)
+    #     # self.assertTrue(result)
+    #     # self.assertEqual(type(result), PhotosResponse)
+
     async def test_search_photos(self):
         photos = [Photo(), Photo(), Photo()]
         mocked_photos = MagicMock()
         mocked_photos.scalars.return_value.all.return_value = photos
-        # result = await search_photos(self.user, "test", 10, 0, self.session)
-        # self.assertTrue(result)
-        # self.assertEqual(type(result), PhotosResponse)
+        self.session.execute.return_value = mocked_photos
+        result = await search_photos("test", 10, 0, self.session, self.user)
+        self.assertTrue(result)
+        self.assertEqual(type(result), type(list()))
 
-    # failed
+    # # failed
+    # async def test_search_photos_by_filter(self):
+    #     photos = [Photo(), Photo(), Photo()]
+    #     mocked_photos = MagicMock()
+    #     mocked_photos.scalars.return_value.all.return_value = photos
+    #     # result = await search_photos_by_filter("test", 0, 5, 10, 0, self.session, self.user)
+    #     # self.assertTrue(result)
+    #     # self.assertEqual(type(result), PhotosResponse)
+
     async def test_search_photos_by_filter(self):
         photos = [Photo(), Photo(), Photo()]
         mocked_photos = MagicMock()
         mocked_photos.scalars.return_value.all.return_value = photos
-        # result = await search_photos_by_filter("test", 0, 5, 10, 0, self.session, self.user)
-        # self.assertTrue(result)
-        # self.assertEqual(type(result), PhotosResponse)
+        self.session.execute.return_value = mocked_photos
+        result = await search_photos_by_filter(
+            "test", 0, 5, 10, 0, self.session, self.user
+        )
+        self.assertTrue(result)
